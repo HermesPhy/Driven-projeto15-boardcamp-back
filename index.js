8d4aaeb4eec62101362f9644293419f7844500c8
Hermes Júnior <hermesj.junior@gmail.com>
2022-05-31 11:25:11 -0300

    Import gamesRouter

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import gamesRouter from "./routes/gamesRouter.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(gamesRouter);
 
 app.listen(process.env.PORT || 4000, () => {
     console.log(`Server running on port ${process.env.PORT || 4000}`)
