356d76a39d3415066611e64a6f302f33e16698f2
Hermes Júnior <hermesj.junior@gmail.com>
2022-05-31 08:32:24 -0300

    Adjustments controllers

---
+++ controllers/gamesController.js
@@ -0,0 +1,27 @@
+import db from "../db.js";
+
+export async function getGame(req, res) {
+    const { queryObject } = res.locals;
+    try {
+        const result = await db.query(queryObject);
+        res.send(result.rows);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export async function postNewGame(req, res) {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    try {
+        const result = await db.query(
+            `INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+            VALUES ($1, $2, $3, $4, $5)`,
+            [name, image, stockTotal, categoryId, pricePerDay]);
+
+        res.sendStatus(201);
+    } catch (err) {
+        if (err.code === "23505") return res.sendStatus(409);
+
+        res.sendStatus(500);
+    }
+}
---
+++ controllers/rentalsController.js
@@ -0,0 +1,66 @@
+import db from "../db.js";
+import formatRentalData from "../utils/formatRentalData.js";
+import getTodayInStringYYYYMMDD from "../utils/getTodayInStringYYYYMMDD.js";
+
+export async function getRentals(req, res) {
+    const { queryObject } = res.locals;
+
+    try {
+        const result = await db.query(queryObject);
+        const formattedData = formatRentalData(result.rows);
+
+        res.send(formattedData);
+    } catch (err) {
+        res.sendStatus(500);
+    }
+}
+
+export async function postNewRental(req, res) {
+    const { customerId, gameId, daysRented } = req.body;
+    let rentDate = getTodayInStringYYYYMMDD();
+  
+    try {
+      const result = await db.query(
+        `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "originalPrice") 
+        VALUES ($1, $2, $3, $4, $4 * (
+        SELECT games."pricePerDay" as "originalPrice" FROM games WHERE games.id = $2))`,
+        [customerId, gameId, rentDate, daysRented]);
+
+        res.sendStatus(201);
+    } catch (err) {
+        if (err.code === "23505") return res.sendStatus(409);
+
+        res.sendStatus(500);
+    }
+}
+
+export async function setRentalAsFinished(req, res) {
+    const { id } = req.params;
+    let returnDate = getTodayInStringYYYYMMDD();
+  
+    try {
+      const result = await db.query(
+        `UPDATE rentals 
+        SET "returnDate" = $1, "delayFee" = GREATEST((($1 - (rentals."rentDate" + rentals."daysRented")) * rentals."originalPrice"),0)
+        WHERE id = $2`,
+        [returnDate, id]);
+
+      res.sendStatus(200);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  }
+
+  export async function deleteRental(req, res) {
+    const { id } = req.params;
+  
+    try {
+      const result = await db.query(`DELETE FROM rentals WHERE id = $1`, [id]);
+  
+      if (result.rowCount === 0) return res.sendStatus(404);
+
+      res.sendStatus(200);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  }
