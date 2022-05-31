abcce89b09c3696cb8b2bae827fcd884ef559af3
Hermes Júnior <hermesj.junior@gmail.com>
2022-05-31 01:08:25 -0300

    First configuration of controllers

---
+++ controllers/categoriesController.js
@@ -0,0 +1,26 @@
+import db from "./../db.js";
+
+export async function getCategories(req, res) {
+    const { queryOptions } = res.locals;
+
+    const text = `SELECT * FROM categories ${queryOptions}`;
+
+    try {
+        const result = await db.query(text);
+        res.send(result.rows);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export async function postNewCategory(req, res) {
+    const { name } = req.body;
+    
+    try {
+        const result = await db.query(`INSERT INTO categories (name) VALUES ($1)`, [name]);
+        res.sendStatus(201);
+      } catch (err) {
+        if (err.code === "23505") return res.sendStatus(409);
+        res.sendStatus(500);
+      }
+}
---
+++ controllers/customersController.js
@@ -0,0 +1,53 @@
+import db from "./../db.js";
+
+export async function getCustomers(req, res) {
+    const { queryObject } = res.locals;
+
+    try {
+        const result = await db.query(queryObject);
+        res.send(result.rows);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export async function getCustomersById(req, res) {
+    const queryObject = res.locals;
+    console.log("queryObject", queryObject);
+
+    try {
+        const result = await db.query(queryObject);
+    
+        if (result.rowCount === 0) return res.sendStatus(404);
+        res.send(result.rows);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export async function postNewCustomer(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        const result = await db.query(
+            `INSERT INTO customers (name, phone, cpf, birthday) 
+            VALUES ($1, $2, $3, $4)`,
+            [name, phone, cpf, birthday]);
+        res.sendStatus(201);
+    } catch (err) {
+        if (err.code === "23505") return res.sendStatus(409);
+        res.sendStatus(500);
+    }
+}
+
+export async function updateCustomer(req, res) {
+    const { queryObject } = res.locals;
+    try {
+      const result = await db.query(queryObject);
+  
+      if (result.rowCount === 0) return res.sendStatus(404);
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
